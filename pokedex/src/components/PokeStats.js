611a78dc3644ecb6f97f4e5746f766e3fc70717f
RaulRueda20 <raulrueda2093@gmail.com>
2022-06-07 10:59:41 -0500

    Crea el componente de estatus

--- pokedex/src/components/PokeStats.js
+++ pokedex/src/components/PokeStats.js
@@ -5,6 +5,19 @@ function PokeStats({ pokeId }) {
   const [pokeStadistics, setPokeStadistics] = useState(null);
   useEffect(() => {
     console.log("pokeId", pokeId);
+    const getPokemonStadistics = async (pokeId) => {
+      if (pokeId !== null) {
+        try {
+          const response = await axios.get(
+            "https://pokeapi.co/api/v2/stat/" + `${pokeId}` + "/"
+          );
+          console.log(response);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    };
+    getPokemonStadistics();
   }, [pokeId]);
   return <div>PokeStats</div>;
 }
